f40cd6b4ac395674f5d99ef4deeeb9206c08ca37
linxz-coder <234692261@qq.com>
2021-08-31 21:20:58 +0800

    changed

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by linxiaozhong on 2021/6/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
